c9903d3a14b4ce76c321a73caacb89811768e396
Adam Purdon <G18P8803@ict.ru.ac.za>
2020-03-09 11:15:23 +0200

    began second tutorial

--- tutorial2/Program.fs
+++ tutorial2/Program.fs
@@ -2,7 +2,45 @@
 
 open System
 
+let parseUnits = function
+    | 0 -> ""
+    | 1 -> "one"
+    | 2 -> "two"
+    | 3 -> "three"
+    | 4 -> "four"
+    | 5 -> "five"
+    | 6 -> "six"
+    | 7 -> "seven"
+    | 8 -> "eight"
+    | 9 -> "nine"
+    | _ -> failwith "I expected a single digit"
+
+let tensName n = 
+    n % 
+
+let parseTens n = 
+    let tens = (n % 100) / 10
+    let units = n % 10
+    match tens with
+    | 0 -> parseUnits units
+    | 1 ->
+        match units with
+        | 0 -> "ten"
+        | 1 -> "eleven"
+        | 2 -> "twelve"
+        | 3 -> "thirteen"
+        | 4 -> "fourteen"
+        | 5 -> "fifteen"
+        | 6 -> "sixteen"
+        | 7 -> "seventeen"
+        | 8 -> "eightteen" 
+        | 9 -> "nineteen"
+        | _ ->
+            failwith "Units cannot be > 9"
+    | n -> 
+        sprintf "%s %s" (tensName n) (parseUnits units)      
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "%f" 45.0 
     0 // return an integer exit code
